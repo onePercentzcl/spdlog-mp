93c6a1ba5292a4a4d9991ecb8bb1bb39ef1bfcf4
OnePercent <onepercentzcl@gmail.com>
2026-01-22 22:53:01 +0800

    feat: 添加 mp2_standalone 独立编译示例

---
+++ example/mp2_standalone/xmake.lua
@@ -0,0 +1,26 @@
+-- xmake.lua for spdlog-mp example_mp2
+-- 从远程仓库下载 spdlog-mp 库
+
+set_project("example_mp2")
+set_version("1.0.0")
+
+-- 设置 C++17 标准
+set_languages("cxx17")
+
+-- 配置编译模式
+add_rules("mode.debug", "mode.release")
+
+-- 设置默认为 release 模式
+set_defaultmode("release")
+
+-- 添加 spdlog-mp 包（从自定义仓库）
+add_repositories("xmake-repo- https://github.com/onePercentzcl/xmake-repo-")
+add_requires("spdlog-mp main", {configs = {enable_multiprocess = true}})
+
+-- example_mp2 程序
+target("example_mp2")
+    set_kind("binary")
+    add_files("example_mp2.cpp")
+    add_packages("spdlog-mp")
+    add_syslinks("pthread", "rt")
+target_end()
